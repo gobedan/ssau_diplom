8e8bc7c39357b7a6668bd7a53e5b63f8cee267b7
Valentin Markin <jobedano@gmail.com>
2021-04-26 10:35:15 +0400

    DB schema done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_165749) do
+ActiveRecord::Schema.define(version: 2021_04_26_062257) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,18 @@ ActiveRecord::Schema.define(version: 2021_04_25_165749) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "orders_products", id: false, force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 8, scale: 2
@@ -58,8 +70,20 @@ ActiveRecord::Schema.define(version: 2021_04_25_165749) do
     t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.string "password"
+    t.string "email"
+    t.string "address"
+    t.integer "zipcode"
+    t.bigint "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "characteristics", "measure_units"
   add_foreign_key "characteristics", "subcategories"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "subcategories"
   add_foreign_key "subcategories", "categories"
 end
---
+++ db/structure.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
+CREATE TABLE IF NOT EXISTS "categories" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS "subcategories" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "category_id" integer NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, CONSTRAINT "fk_rails_3937626525"
+FOREIGN KEY ("category_id")
+  REFERENCES "categories" ("id")
+);
+CREATE INDEX "index_subcategories_on_category_id" ON "subcategories" ("category_id");
+CREATE TABLE IF NOT EXISTS "measure_units" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
+CREATE TABLE IF NOT EXISTS "characteristics" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "measure_unit_id" integer, "subcategory_id" integer, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, CONSTRAINT "fk_rails_4cf7b3f51e"
+FOREIGN KEY ("measure_unit_id")
+  REFERENCES "measure_units" ("id")
+, CONSTRAINT "fk_rails_ead16bd1d4"
+FOREIGN KEY ("subcategory_id")
+  REFERENCES "subcategories" ("id")
+);
+CREATE INDEX "index_characteristics_on_measure_unit_id" ON "characteristics" ("measure_unit_id");
+CREATE INDEX "index_characteristics_on_subcategory_id" ON "characteristics" ("subcategory_id");
+CREATE TABLE IF NOT EXISTS "products" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "price" decimal(8,2), "subcategory_id" integer NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, "picture_url" varchar, CONSTRAINT "fk_rails_150008c07d"
+FOREIGN KEY ("subcategory_id")
+  REFERENCES "subcategories" ("id")
+);
+CREATE INDEX "index_products_on_subcategory_id" ON "products" ("subcategory_id");
+CREATE TABLE IF NOT EXISTS "characteristics_products" ("characteristic_id" integer NOT NULL, "product_id" integer NOT NULL, "value" varchar);
+CREATE TABLE IF NOT EXISTS "users" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "login" varchar, "password" varchar, "email" varchar, "address" varchar, "zipcode" integer, "phone" bigint, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
+CREATE TABLE IF NOT EXISTS "orders" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "user_id" integer NOT NULL, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, CONSTRAINT "fk_rails_f868b47f6a"
+FOREIGN KEY ("user_id")
+  REFERENCES "users" ("id")
+);
+CREATE INDEX "index_orders_on_user_id" ON "orders" ("user_id");
+CREATE TABLE IF NOT EXISTS "orders_products" ("order_id" integer NOT NULL, "product_id" integer NOT NULL);
+INSERT INTO "schema_migrations" (version) VALUES
+('20210425071557'),
+('20210425071617'),
+('20210425071709'),
+('20210425071818'),
+('20210425072335'),
+('20210425082129'),
+('20210425165749'),
+('20210426061814'),
+('20210426062130'),
+('20210426062257');
+
+
